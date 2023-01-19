e62d8413133f3fe90c717838a42b30f41b58b173
Mwangi Micha <michameiu@gmail.com>
2023-01-19 10:39:45 +0300

    Implement digital ocean droplet search get

--- dist/index.js
+++ dist/index.js
@@ -16067,7 +16067,6 @@ try {
             // always executed
         });
 
-    // core.setOutput("ip_addresses", "[1.1, 2.2, 3.3]");
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
